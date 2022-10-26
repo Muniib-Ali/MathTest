1b6bd02026d40612f5e28b1d6ebefcffc09e3c65
fahm781 <farazahmed9910@gmail.com>
2022-10-26 09:40:51 +0100

    Added sum and divide functions

--- Math.java/Math.java
+++ Math.java/Math.java
@@ -6,7 +6,24 @@ public class Math{
 
     }
 
+    /********************************
+Developer: Faraz Ahmed
+University ID: 210141722
+Function: This function takes two inputs as integers and returns the sum
+********************************/
 
+public int add(int a, int b){
+return a+b;
+}
 
+/********************************
+Developer: Faraz Ahmed
+University ID: 210141722
+Function: This function takes two inputs as integers and returns the subtraction.
+********************************/
+
+public int divide(int a , int b){
+return a+b;
+}
 
 }
