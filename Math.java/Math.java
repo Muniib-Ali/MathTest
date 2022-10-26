6ade7e43d8f800f8204d1dfc0738b0c460d1ba8e
Muniib Ali <muniib11@live.com>
2022-10-26 09:34:47 +0100

    Added the max and min functions

--- Math.java/Math.java
+++ Math.java/Math.java
@@ -7,6 +7,23 @@ public class Math{
     }
 
 
+public int max(int a, int b){
+if(a > b){
+return a;
+} else {
+return b;
+}
+}
+
+public int min(int a, int b){
+if(a > b){
+return b;
+} else {
+return a;
+}
+}
+
+
 
 
 }
